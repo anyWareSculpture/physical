be9c760474ef5ed91b64b42b608bc7423da37b11
Marius Kintel <marius@kintel.net>
2016-06-13 15:27:14 -0400

    Updated to use new disk sensors; ring of IR steps rather than a single hall effect sensor

--- arduino/DiskInterface/DiskInterface.ino
+++ arduino/DiskInterface/DiskInterface.ino
@@ -5,18 +5,17 @@
 
   Example commands:
 
+HELLO
 DISK-RESET
-DISK-STATE success
-DISK 0 POS 10 DIR 1 USER 1
-DISK 1 POS 50 DIR 1 USER 1
-DISK 2 POS 60 DIR 1 USER 2
+DISK 0 POS 10 DIR 1
+DISK 1 POS 50 DIR 1
+DISK 2 POS 60 DIR 1
 DISK 0 DIR 0 USER 1
 DISK 0 DIR 1 USER 1
 DISK 0 DIR -1 USER 1
 DISK 0 DIR 1 USER 1
 DISK 1 DIR -1 USER 1
 DISK 2 DIR 1 USER 1
-DISK-EXIT
 
 */
 
@@ -24,37 +23,38 @@ DISK-EXIT
 #error Oops!  Make sure you have 'Teensy 3.1' selected from the 'Tools -> Board' menu.
 #endif
 
-#include "./Timer.h"
-#include "./Bounce2.h"
 #include "DiskInterface.h"
 #include "anyware_serial.h"
 
 // Set to 1 to start in debug mode
 #define DEBUG_MODE 0
 
+// All disks
+const int STEPS_PER_REVOLUTION = 180;
+
 // Bottom disk
-const int DISK0_HOME_SENSOR = A0;
-const int DISK0_ENC_A = 5;
-const int DISK0_ENC_B = 6;
-const int DISK0_MOTOR_A = 4;
-const int DISK0_MOTOR_B = 3;
-const int DISK0_TICKS_PER_REVOLUTION = 30089;
+const int DISK0_STEP_SENSOR = 2;
+const int DISK0_ENC_A = 17;
+const int DISK0_ENC_B = 16;
+const int DISK0_MOTOR_A = 21;
+const int DISK0_MOTOR_B = 20;
+const int DISK0_TICKS_PER_STEP = 36;
 
 // Middle disk
-const int DISK1_HOME_SENSOR = A1;
-const int DISK1_ENC_A = 9;
-const int DISK1_ENC_B = 10;
-const int DISK1_MOTOR_A = 8;
-const int DISK1_MOTOR_B = 7;
-const int DISK1_TICKS_PER_REVOLUTION = 31348;
+const int DISK1_STEP_SENSOR = 1;
+const int DISK1_ENC_A = 11;
+const int DISK1_ENC_B = 12;
+const int DISK1_MOTOR_A = 3;
+const int DISK1_MOTOR_B = 5;
+const int DISK1_TICKS_PER_STEP = 56;
 
 // Top disk
-const int DISK2_HOME_SENSOR = A2;
-const int DISK2_ENC_A = 20;
-const int DISK2_ENC_B = 19;
-const int DISK2_MOTOR_A = 17;
-const int DISK2_MOTOR_B = 18;
-const int DISK2_TICKS_PER_REVOLUTION = 31385;
+const int DISK2_STEP_SENSOR = 0;
+const int DISK2_ENC_A = 19;
+const int DISK2_ENC_B = 18;
+const int DISK2_MOTOR_A = 22;
+const int DISK2_MOTOR_B = 23;
+const int DISK2_TICKS_PER_STEP = 36;
 
 // Globals
 
@@ -64,48 +64,43 @@ const uint8_t MAGNET_SENSITIVITY = 160;
 #define USER1_STR "1"
 #define USER2_STR "2"
 
-Timer timer;
-int8_t homingIdx = -1;
-
 struct Disk {
   int id;
-  int homeSensor;
+  int stepSensor;
   int enca, encb;
   int motorA,motorB;
-  uint32_t ticksPerRevolution;
+  uint16_t ticksPerStep;
 
   void (*enca_isr)();
   void (*encb_isr)();
+  void (*step_isr)();
 
   bool enca_val;
   bool encb_val;
+  bool step_val;
 
-  int magnetValue;
-  int lastMagnetValue;
-
-  uint16_t targetPosition;
+  int16_t targetPosition;
   uint16_t actualPosition;
-  int32_t tickCount;
-  bool posChanged;
+  int16_t tickCount;
+  int16_t lastTickCount;
+  int16_t tickDir;
+  int16_t stepCount;
 
   Direction direction;
 
   static const int DISK_READY = 0;
   static const int DISK_HOMING = 1;
   int state;
+  uint8_t home;
 
-  int32_t lastExitTicks;
-  int32_t lastHitTicks;
-
-  Disk(int diskid, int hs, int enca, int encb, int ma, int mb, uint32_t ticks, void (*enca_isr)(), void (*encb_isr)()) :
-    id(diskid), homeSensor(hs), enca(enca), encb(encb), motorA(ma), motorB(mb),
-    ticksPerRevolution(ticks),
-    enca_isr(enca_isr), encb_isr(encb_isr),
+  Disk(int diskid, int stepsensor, int enca, int encb, int ma, int mb, uint16_t ticks,
+       void (*enca_isr)(), void (*encb_isr)(), void (*step_isr)()) :
+    id(diskid), stepSensor(stepsensor), enca(enca), encb(encb), motorA(ma), motorB(mb),
+    ticksPerStep(ticks),
+    enca_isr(enca_isr), encb_isr(encb_isr), step_isr(step_isr),
     enca_val(false), encb_val(false),
-    magnetValue(0), lastMagnetValue(0),
-    actualPosition(0), tickCount(0), posChanged(false),
-    direction(DIR_OFF), state(DISK_READY),
-    lastExitTicks(0), lastHitTicks(0)
+    actualPosition(0), tickCount(0), tickDir(DIR_OFF), lastTickCount(0), stepCount(0),
+    direction(DIR_OFF), state(DISK_READY), home(0)
   {
   }
 
@@ -114,33 +109,62 @@ struct Disk {
     pinMode(motorB, OUTPUT);
     pinMode(enca, INPUT_PULLUP);
     pinMode(encb, INPUT_PULLUP);
+    pinMode(stepSensor, INPUT_PULLUP);
     enca_val = digitalRead(enca);
     encb_val = digitalRead(encb);
     reset();
     attachInterrupt(enca, enca_isr, CHANGE);
     attachInterrupt(encb, encb_isr, CHANGE);
+    attachInterrupt(stepSensor, step_isr, CHANGE);
   }
 
   void reset() {
     targetPosition = -1;
+    Serial.println("Reset");
     direction = DIR_OFF;
   }
 
-  inline uint16_t ticksToPos(int32_t ticks) {
-    if (ticks < 0) ticks += ticksPerRevolution;
-    return (ticks * 360 / ticksPerRevolution) % 360;
+  inline uint16_t calcPos() {
+    return (stepCount * 360 / STEPS_PER_REVOLUTION + tickCount * (360 / STEPS_PER_REVOLUTION) / ticksPerStep + 360)%360;
   }
   
   inline void handleEncA() {
     enca_val = digitalRead(enca);
-    if (enca_val != encb_val) tickCount = (tickCount-1+ticksPerRevolution)%ticksPerRevolution;
-    else tickCount = (tickCount+1)%ticksPerRevolution;
+    if (enca_val != encb_val) {
+      tickCount++;
+      tickDir = DIR_CCW;
+    }
+    else {
+      tickCount--;
+      tickDir = DIR_CW;
+    }
   }
 
   inline void handleEncB() {
     encb_val = digitalRead(encb);
-    if (enca_val == encb_val) tickCount = (tickCount-1+ticksPerRevolution)%ticksPerRevolution;
-    else tickCount = (tickCount+1)%ticksPerRevolution;
+    if (enca_val == encb_val) {
+      tickCount++;
+      tickDir = DIR_CCW;
+    }
+    else {
+      tickCount--;
+      tickDir = DIR_CW;
+    }
+  }
+
+  inline void handleStep() {
+    if (abs(tickCount) > ticksPerStep * 2) {
+      if (tickCount > 0) stepCount = 0;
+      else stepCount = STEPS_PER_REVOLUTION-3;
+      tickCount = 0;
+      if (state == DISK_HOMING) home++;
+    }
+    else {
+      if (tickDir == DIR_CCW) stepCount++;
+      else if (tickDir == DIR_CW) stepCount--;
+      lastTickCount = tickCount;
+      tickCount = 0;
+    }
   }
 
   void setState(int s) {
@@ -153,7 +177,8 @@ struct Disk {
       break;
     case DISK_HOMING:
       Serial.println(" homing");
-      direction = DIR_CW;
+      setTargetPosition(-1, DIR_CW);
+      home = 0;
       break;
     }
   }
@@ -174,14 +199,6 @@ struct Disk {
     return actualPosition;
   }
 
-  void readSensors() {
-    magnetValue = analogRead(homeSensor);
-    // if (magnetValue < 300 && magnetValue > 20) {
-    //   Serial.print(id);Serial.print(" ");
-    //   Serial.print(magnetValue);Serial.println();
-    // }
-  }
-
   void motorMove() {
     switch (direction) {
     case DIR_CW:
@@ -199,22 +216,15 @@ struct Disk {
     }
   }
 
-  bool magnetActive(int magnetValue) {
-    return magnetValue < MAGNET_SENSITIVITY;
-  }
-
-  bool isHome() {
-    return (magnetActive(magnetValue) && !magnetActive(lastMagnetValue));
-  }
-
   void handleCount() {
-    uint16_t pos = ticksToPos(tickCount);
+    uint16_t pos = calcPos();
     if (pos != actualPosition) {
       actualPosition = pos;
       if (global_state == STATE_READY) {
         Serial.print("DISK ");Serial.print(id);
         Serial.print(" POS ");Serial.print(actualPosition);
         Serial.print(" DIR ");Serial.print(direction);
+        Serial.print(" STEPS ");Serial.print(stepCount);
         Serial.print(" TICKS ");Serial.print(tickCount);
         Serial.println();
       }
@@ -222,13 +232,11 @@ struct Disk {
   }
 
   void manage() {
-
-    // Read local sensors
-    readSensors();
-
     if (state == DISK_HOMING) {
-      if (isHome()) {
-        tickCount = 0;
+      if (home == 1) {
+        direction = DIR_CCW;
+      }
+      else if (home == 2) {
         actualPosition = 0;
         setState(DISK_READY);
       }
@@ -237,33 +245,12 @@ struct Disk {
       handleCount();
 
       if (targetPosition >= 0 && actualPosition == targetPosition) {
+        Serial.println("at wanted position");
         direction = DIR_OFF;
         targetPosition = -1;
       }
-
-      if (magnetActive(magnetValue) && !magnetActive(lastMagnetValue)) {
-        // Hit left side of magnet
-        int32_t tmpticks = tickCount;
-        if (global_state == STATE_READY && this->direction != DIR_OFF) {
-          Serial.print("DEBUG DISK ");
-          Serial.print(id);
-          Serial.print(" HIT TICKS " );
-          Serial.println(tmpticks);
-        }
-        lastHitTicks = tmpticks;
-      }
-      else if (!magnetActive(magnetValue) && magnetActive(lastMagnetValue)) {
-        // Exited left side of magnet
-        // Serial.print(id);
-        // Serial.print(" EXIT TICKS " );
-        // Serial.println(tickCount);
-        lastExitTicks = tickCount;
-      }
-
     }
     motorMove();
-
-    lastMagnetValue = magnetValue;
   }
 };
 
@@ -279,6 +266,11 @@ void disk0b_isr() {
   disk[0].handleEncB();
   sei();
 }
+void disk0step_isr() {
+  cli();
+  disk[0].handleStep();
+  sei();
+}
 void disk1a_isr() {
   cli();
   disk[1].handleEncA();
@@ -287,6 +279,11 @@ void disk1a_isr() {
 void disk1b_isr() {
   cli();
   disk[1].handleEncB();
+  sei();
+}
+void disk1step_isr() {
+  cli();
+  disk[1].handleStep();
   sei();
 }
 void disk2a_isr() {
@@ -299,29 +296,31 @@ void disk2b_isr() {
   disk[2].handleEncB();
   sei();
 }
+void disk2step_isr() {
+  cli();
+  disk[2].handleStep();
+  sei();
+}
 
 Disk disk[3] = {
-  Disk(0, DISK0_HOME_SENSOR, DISK0_ENC_A, DISK0_ENC_B, DISK0_MOTOR_A, DISK0_MOTOR_B, DISK0_TICKS_PER_REVOLUTION, &disk0a_isr, &disk0b_isr),
-  Disk(1, DISK1_HOME_SENSOR, DISK1_ENC_A, DISK1_ENC_B, DISK1_MOTOR_A, DISK1_MOTOR_B, DISK1_TICKS_PER_REVOLUTION, &disk1a_isr, &disk1b_isr),
-  Disk(2, DISK2_HOME_SENSOR, DISK2_ENC_A, DISK2_ENC_B, DISK2_MOTOR_A, DISK2_MOTOR_B, DISK2_TICKS_PER_REVOLUTION, &disk2a_isr, &disk2b_isr)
+  Disk(0, DISK0_STEP_SENSOR, DISK0_ENC_A, DISK0_ENC_B, DISK0_MOTOR_A, DISK0_MOTOR_B, DISK0_TICKS_PER_STEP, &disk0a_isr, &disk0b_isr, &disk0step_isr),
+  Disk(1, DISK1_STEP_SENSOR, DISK1_ENC_A, DISK1_ENC_B, DISK1_MOTOR_A, DISK1_MOTOR_B, DISK1_TICKS_PER_STEP, &disk1a_isr, &disk1b_isr, &disk1step_isr),
+  Disk(2, DISK2_STEP_SENSOR, DISK2_ENC_A, DISK2_ENC_B, DISK2_MOTOR_A, DISK2_MOTOR_B, DISK2_TICKS_PER_STEP, &disk2a_isr, &disk2b_isr, &disk2step_isr)
 };
 
 void setGlobalState(int state) {
   if (state == STATE_HOMING) {
     global_state = STATE_BLOCKED; // Don't output anything before we start homing
-    // Move out of home zone for a bit to make sure we re-trigger the sensors
-    for (int i=0;i<3;i++) disk[i].setTargetPosition(-1, DIR_CCW);
-    homingIdx = timer.after(2000, startHoming, NULL);
+    startHoming();
   }
   else {
     global_state = state;
   }
 }
 
-void startHoming(void *context) {
+void startHoming() {
   global_state = STATE_HOMING;
   for (int i=0;i<3;i++) disk[i].setState(Disk::DISK_HOMING);
-  homingIdx = -1;
 }
 
 void manageDiskGame() {
@@ -331,11 +330,6 @@ void manageDiskGame() {
       if (disk[i].getState() == Disk::DISK_HOMING) homed = false;
     }
     if (homed) {
-      // for (int i=0;i<3;i++) {
-      //   Serial.print(disk[i].id);
-      //   Serial.print(" HOME TICKS ");
-      //   Serial.println(disk[i].tickCount);
-      // }
       setGlobalState(STATE_READY);
     }
   }
@@ -391,7 +385,6 @@ void loop()
 {
   handleSerial();
 
-  timer.update();
   manageDiskGame();
 }
 
