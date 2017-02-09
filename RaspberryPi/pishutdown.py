5ee03ab13c54824b1244fd9bbc579c88d6d673e5
Marius Kintel <marius@kintel.net>
2017-02-09 18:53:34 -0500

    Debounced shutdown button

--- RaspberryPi/pishutdown.py
+++ RaspberryPi/pishutdown.py
@@ -6,9 +6,10 @@ import syslog
 import os
 import sys
 import signal
+from time import sleep
 
 def cleanup(signum, frame):
-   print 'pishutdown: Caught a signal, shutting down.\n'
+   syslog.syslog('pishutdown: Caught a signal, shutting down')
    gpio.cleanup()
    sys.exit(1)
 
@@ -19,19 +20,26 @@ gpio.setmode(gpio.BOARD)
 
 gpio.setup(38, gpio.OUT)
 gpio.output(38, gpio.HIGH)
-gpio.setup(40, gpio.IN)
+# Enable pull-down for safety
+gpio.setup(40, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 while True:
   try:
     gpio.wait_for_edge(40, gpio.RISING)
-    # Shutdown && log
-    syslog.syslog('pishutdown: Soft shutdown');
-    gpio.cleanup();
-    os.system('shutdown now -h')
+    # Debounce
+    for i in range(10):
+	sleep(0.010)
+	if not gpio.input(40): break
+    if i == 9:
+        # Shutdown && log
+	syslog.syslog('pishutdown: Soft shutdown')
+	gpio.cleanup()
+    	os.system('shutdown now -h')
+    	sys.exit(0)
   except KeyboardInterrupt:  
     print 'Keyboard interrupt'
-    gpio.cleanup();
+    gpio.cleanup()
     sys.exit(1)    
-  except:
-    syslog.syslog('pishutdown: Exception');
+  except Exception as e:
+    syslog.syslog('pishutdown: Exception: ' + str(e))
 
